d1443c98faa0cc92c53ae71e5a865ee2c793587c
user <msazad202@gmail.com>
2024-09-01 23:16:40 +0530

    added cronjob

---
+++ src/cronjob.go
@@ -0,0 +1,15 @@
+package cronjob
+
+import (
+	"web-api/src/service"
+
+	"github.com/robfig/cron/v3"
+)
+
+func StartCronJob() {
+	c := cron.New()
+	c.AddFunc("@every 5m", func() {
+		service.FetchAndStoreCryptocurrencyData()
+	})
+	c.Start()
+}

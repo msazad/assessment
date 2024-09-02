9465b0f1f2bd926c27b3bdba1e4e5d246d0b79c9
user <msazad202@gmail.com>
2024-09-03 02:15:55 +0530

    added the repository layer

---
+++ src/repository/cryptocurrency_repository.go
@@ -0,0 +1,23 @@
+package repository
+
+import (
+
+	"github.com/msazad/assessment/src/models"
+	"github.com/msazad/assessment/utils/database"
+)
+
+func GetAllCryptocurrencies() []models.Cryptocurrency {
+	var cryptos []models.Cryptocurrency
+	database.DB.Find(&cryptos)
+	return cryptos
+}
+
+func SearchCryptocurrencies(term string) ([]models.Cryptocurrency, error) {
+	var cryptocurrencies []models.Cryptocurrency
+	query := "%" + term + "%"
+	err := database.DB.Where("name ILIKE ? OR symbol ILIKE ?", query, query).Find(&cryptocurrencies).Error
+	if err != nil {
+		return nil, err
+	}
+	return cryptocurrencies, nil
+}
